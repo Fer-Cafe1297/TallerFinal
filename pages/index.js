bb3d9f6109021cc074d225e381b96b972233f03d
Fer-Cafe1297 <82001585+Fer-Cafe1297@users.noreply.github.com>
2022-05-07 23:35:07 -0600

    carrusel informativo

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from '../styles/HomePage.module.css'
 import LayoutComponent from '../components/layout/LayoutComponent'
 import CaroulselComponent from '../components/CaroulselComponent'
 import ServicesComponent from '../components/ServicesComponent'
+import CarouselInformative from '../components/CarouselInformative'
 
 export const Home = () => {
   return (
@@ -9,6 +10,7 @@ export const Home = () => {
       <div className={styles.container}>
         <CaroulselComponent />
         <ServicesComponent/> 
+        <CarouselInformative/>
       </div>
     </LayoutComponent>
 
